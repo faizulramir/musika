9ad2854cd1a297f37cef8c531730f21d7004359f
Faizul <fzlxtech>
2023-12-09 07:10:30 +0800

    update chat

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let app = require('express')();
 let server = require('http').createServer(app);
 let io = require('socket.io')(server, {
     cors: {
-      origin: "http://localhost:8100",
+      origin: ["http://localhost:8100", "http://192.168.100.2:8100"],
       credentials: true
     }
   });
@@ -10,31 +10,48 @@ const cors = require('cors');
 app.use(cors());
 
 io.on('connection', (socket) => {
-    socket.on('disconnect', function(){
-      io.emit('usersActivity', {
-        user: socket.username,
-        event: 'chatLeft'
-      });
-    });
-   
-    socket.on('setUserName', (name) => {
-      socket.username = name;
-      io.emit('usersActivity', {
-        user: name,
-        event: 'chatJoined'
-      });    
-    });
-    
-    socket.on('sendTheMessage', (message) => {
-      io.emit('message', {
-        msg: message.text,
-        user: socket.username,
-        createdAt: new Date()
-      });    
-    });
+  console.log("new connection made")
+  socket.on('disconnect', function(){
+    // io.emit('usersActivity', {
+    //   user: socket.username,
+    //   event: 'chatLeft'
+    // });
+    console.log('discconect')
   });
 
-let port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log('Server Started');
+  socket.on('setOffline', (name) => {
+    socket.username = name;
+    console.log('offline')
+    io.emit('userStatus', {
+      user: socket.username,
+      event: 'chatLeft'
+    });   
+  });
+
+  socket.on('setOnline', (name) => {
+    socket.username = name;
+    io.emit('userStatus', {
+      user: name,
+    });    
+  });
+  
+  socket.on('setUserName', (name) => {
+    socket.username = name;
+    io.emit('usersActivity', {
+      user: name,
+      event: 'chatJoined'
+    });    
+  });
+  
+  socket.on('sendTheMessage', (message) => {
+    io.emit('message', {
+      msg: message.text,
+      user: socket.username,
+      createdAt: new Date()
+    });    
+  });
 });
+
+let port = process.env.PORT || 3000;
+let ipAddress = '192.168.100.2'
+server.listen(port, ipAddress);
